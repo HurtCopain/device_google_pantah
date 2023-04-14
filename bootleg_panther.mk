56243955bed94787695ab152f3d5e348edd290b4
hurtcopain <jacksummers715@gmail.com>
2023-04-14 16:08:54 -0400

    bootleggers

--- bootleg_panther.mk
+++ bootleg_panther.mk
@@ -1,23 +1,19 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
-TARGET_FLATTEN_APEX := false
-TARGET_INCLUDE_CARRIER_SETTINGS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_BLUR := true
+#WITH_GMS := true
+WITH_GAPPS := true
 
-TARGET_INCLUDE_CAMERA_GO := true
-TARGET_SUPPORTS_LILY_EXPERIENCE := true
-TARGET_INCLUDE_ADDITIONAL_GAPPS := true
-
-# Inherit from telephony config
-$(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
-
-# Inherit from apex config
-$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
-
-# Inherit from gapps config
-$(call inherit-product, vendor/pixel-additional/gapps/config.mk)
+# Inherit some common Bootleggers ROM stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
@@ -28,7 +24,7 @@ $(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
 $(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := custom_panther
+PRODUCT_NAME := bootleg_panther
 PRODUCT_MODEL := Pixel 7
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
